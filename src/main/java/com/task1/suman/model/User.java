1720f307d27d7c6aa0d085751651246aa5477b62
Suman Prabhu <sumanprabhu268@gmail.com>
2026-02-12 12:21:43 +0530

    modified: one-to-one and many-to many relations between the tables

--- src/main/java/com/task1/suman/model/User.java
+++ src/main/java/com/task1/suman/model/User.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -16,8 +17,23 @@ public class User {
     @Id
     @GeneratedValue
     private UUID id;
+
     private String name;
+
     @Column(nullable = false,unique = true)
     private String email;
+
     private String contactNum;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns=@JoinColumn(name = "user_id"),
+            inverseJoinColumns=@JoinColumn(name = "role_id")
+    )
+    private List<Role> roles;
 }
